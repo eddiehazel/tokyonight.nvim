57b4ff57f10821237b1550d2e040e556460de477
Eddie Hazel <not@email.com>
2024-09-11 06:54:52 +0100

    feat: make night darker

--- lua/tokyonight/colors/night.lua
+++ lua/tokyonight/colors/night.lua
@@ -2,6 +2,6 @@ local ret = vim.deepcopy(require("tokyonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  bg = "#1a1b26",
-  bg_dark = "#16161e",
+  bg = "#16161e",
+  bg_dark = "#000000",
 })
